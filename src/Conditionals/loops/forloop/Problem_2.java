ec73c90f5c9d59e39860dd6998f33c7a4a62ce86
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-06 13:48:16 +0500

    for loops practice Problem_2

--- src/Conditionals/loops/forloop/Problem_2.java
+++ src/Conditionals/loops/forloop/Problem_2.java
@@ -14,7 +14,7 @@ public class Problem_2 {
     public static void main(String[] args) {
         int[] nums = {10, 20, 30, 40, 50};
         for (int i = nums.length - 1; i >= 0; i--) {
-            System.out.println(nums[i]);
+            System.out.println(nums[i] + " ");
         }
     }
 }
