21bfa047504b61e617748193143b4efc601cca1c
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-07 22:46:36 +0500

    Solving Problem_3 of for loops

---
+++ src/Conditionals/loops/forloop/Problem_3.java
@@ -0,0 +1,38 @@
+/*
+Problem 4 — Filtering data (very common)
+- Given a list of integers:
+- Ignore negative numbers
+- Print only even numbers
+- Stop after printing 5 valid numbers
+
+👉 Must use:
+- continue
+- break
+
+👉 Ask yourself:
+- Where should filtering happen?
+- Is this readable?
+*/
+
+package Conditionals.loops.forloop;
+
+public class Problem_3 {
+    public static void main(String[] args) {
+        int[] list = {
+                -10, 3, 4, -7, 8,
+                15, 22, -1, 6, 9,
+                12, -5, 14, 17, -20,
+                18, 21, 24, -3, 30
+        };
+
+        int validCount = 0;
+        for(int i:list) {
+            if (validCount == 5) break;
+            if(i % 2 == 0 && !(i < 0))
+            {
+                System.out.print(i + " ");
+                validCount++;
+            }
+        }
+    }
+}
