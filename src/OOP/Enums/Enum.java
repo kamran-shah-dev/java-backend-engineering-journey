e04149bb40fac28e3ee97e3522bbc7bcdc05b14d
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-12 16:52:40 +0500

    Enum practice pre class and object practice

--- src/OOP/Enums/Enum.java
+++ src/OOP/Enums/Enum.java
@@ -1,4 +1,32 @@
 package OOP.Enums;
 
+enum orderStatus{
+    Paid,
+    Pending,
+    Cancelled,
+    Shipped
+}
 public class Enum {
+
+    static void paymentStatus(orderStatus order) {
+        switch (order) {
+            case Paid:
+                System.out.println("Your payment is completed");
+                break;
+            case Pending:
+                System.out.println("Your order is pending");
+                break;
+            case Cancelled:
+                System.out.println("Your order is cancelled");
+                break;
+            case Shipped:
+                System.out.println("Your order is shipped");
+                break;
+            default:
+                System.out.println("Error");
+        }
+    }
+    public static void main(String[] args) {
+        paymentStatus(orderStatus.Paid);
+    }
 }
