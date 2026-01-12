48c442fe8c37ed066f29a351dbe2714645ff3fb3
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-12 16:59:27 +0500

    Enum practice pre class and object practice with advanced switch

--- src/OOP/Enums/Enum.java
+++ src/OOP/Enums/Enum.java
@@ -10,20 +10,10 @@ public class Enum {
 
     static void paymentStatus(orderStatus order) {
         switch (order) {
-            case Paid:
-                System.out.println("Your payment is completed");
-                break;
-            case Pending:
-                System.out.println("Your order is pending");
-                break;
-            case Cancelled:
-                System.out.println("Your order is cancelled");
-                break;
-            case Shipped:
-                System.out.println("Your order is shipped");
-                break;
-            default:
-                System.out.println("Error");
+            case Paid -> System.out.println("Your payment is completed");
+            case Pending -> System.out.println("Your order is pending");
+            case Cancelled -> System.out.println("Your order is cancelled");
+            case Shipped -> System.out.println("Your order is shipped");
         }
     }
     public static void main(String[] args) {
