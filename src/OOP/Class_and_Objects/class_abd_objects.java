f27c18101a2d8ea46ed598713ef2bc1cb17f8d46
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-15 20:42:03 +0500

    Initializer blocks

--- src/OOP/Class_and_Objects/class_abd_objects.java
+++ src/OOP/Class_and_Objects/class_abd_objects.java
@@ -1,4 +1,22 @@
 package OOP.Class_and_Objects;
 
+class initializer_block {
+    public static int age;
+    public float height;
+    static {
+        age = 10;
+        System.out.println(age);
+    }
+
+    {
+        height = 6.1f;
+        System.out.println(age);
+        System.out.println(height);
+    }
+
+}
 public class class_abd_objects {
+    public static void main(String[] args) {
+        initializer_block ib = new initializer_block();
+    }
 }
--- src/OOP/Records/records.java
+++ src/OOP/Records/records.java
@@ -1,6 +1,9 @@
 package OOP.Records;
+interface users {
 
-record user (String name, int age, float height) {
+}
+
+record user (String name, int age, float height)  {
     public void displayUser()
     {
         System.out.println("Name is: " + this.name);
@@ -13,6 +16,7 @@ public class records {
     public static void main(String[] args) {
         user u = new user("Kamran", 23, 5.9f);
         u.displayUser();
+        System.out.println(u.hashCode());
     }
 }
 
