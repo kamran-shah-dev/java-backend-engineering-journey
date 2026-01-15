edd03437696e15bb67f2125f320cc414109c954c
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-15 19:12:11 +0500

    Exploring java Records

---
+++ src/OOP/Records/records.java
@@ -0,0 +1,18 @@
+package OOP.Records;
+
+record user (String name, int age, float height) {
+    public void displayUser()
+    {
+        System.out.println("Name is: " + this.name);
+        System.out.println("Age is: " + this.age);
+        System.out.println("Height is: " + this.height);
+    }
+}
+
+public class records {
+    public static void main(String[] args) {
+        user u = new user("Kamran", 23, 5.9f);
+        u.displayUser();
+    }
+}
+
