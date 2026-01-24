3adce954f9eb63827ffceb1f6485677c318bfd87
kamranShah-Dev <mrbukhari117@gmail.com>
2026-01-24 22:22:12 +0500

    Lambda expressions with functional interface

---
+++ src/OOP/Lambda_Expressions/functional_interface.java
@@ -0,0 +1,28 @@
+package OOP.Lambda_Expressions;
+
+interface returnWithSymbol {
+    String run(String symbol);
+}
+public class functional_interface {
+    public static void main(String[] args) {
+
+        int number1 = 6;
+        int number2 = 7;
+
+        returnWithSymbol add = (s) -> s + " + ";
+        returnWithSymbol sub = (s) -> s + " - ";
+        returnWithSymbol mul = (s) -> s + " x ";
+
+        simpleCalc(number1, number2, add);
+        simpleCalc(number1, number2, sub);
+        simpleCalc(number1, number2, mul);
+    }
+
+    static void simpleCalc(int num1, int num2, returnWithSymbol s) {
+        String expression = s.run(Integer.toHexString(num1)) + " " + num2;
+
+        if(expression.contains("+")) System.out.println(expression + " = " + (num1 + num2));
+        else if (expression.contains("-")) System.out.println(expression + " = " + (num1 - num2));
+        else System.out.println(expression + " = " + (num1 * num2));
+    }
+}
--- src/OOP/Lambda_Expressions/lambdaExpression.java
+++ src/OOP/Lambda_Expressions/lambdaExpression.java
@@ -1,6 +1,7 @@
 package OOP.Lambda_Expressions;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class lambdaExpression {
     public static void main(String[] args) {
@@ -9,8 +10,12 @@ public class lambdaExpression {
         for (int i = 0; i < 6; i++) numbs.add(i);
 
         // numbs.forEach(System.out::println); // method reference
-        numbs.forEach((n) -> { // lambda expression
-            System.out.println(n);
-        });
+        // numbs.forEach((n) -> { // lambda expression
+        //    System.out.println(n);
+        // });
+
+        Consumer<Integer> numbers = System.out::println; // lambda expression can be stored in a variable
+
+        numbs.forEach(numbers);
     }
 }
