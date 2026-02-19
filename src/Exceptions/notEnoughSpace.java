d04f82d1f9fd2073809538932901de24406dc893
kamranShah-Dev <mrbukhari117@gmail.com>
2026-02-19 16:48:13 +0500

    Exception Handling Creating Custom Exceptions Part 2

--- src/Exceptions/notEnoughSpace.java
+++ src/Exceptions/notEnoughSpace.java
@@ -2,11 +2,11 @@ package Exceptions;
 
 public class notEnoughSpace extends Exception {
     public notEnoughSpace() {
-//        super("Not Enough Space");
+        super("Not Enough Space"); // recommended method
     }
-    @Override
-    public String toString() {
-        return "Not Enough Space";
-    }
+//    @Override
+//    public String toString() {
+//        return "Not Enough Space";
+//    }
 
 }
