2f2de83079e349b2b44d286df75cd275e0310811
kamranShah-Dev <mrbukhari117@gmail.com>
2026-03-16 16:23:51 +0500

    ArrayLists.java - learn arraylist in depth

---
+++ src/Collections/List/ArrayLists.java
@@ -0,0 +1,31 @@
+package Collections.List;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        // adding elements
+        list.add(10);
+        list.add(20);
+        list.add(30);
+
+        /*System.out.println(list.get(2)); // getting single element
+
+        // print arraylist as a list
+        System.out.println(list);
+
+        // accessing elements using pointer.
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        // using enhanced for loop (foreach loop)
+        for(Integer i: list) {
+            System.out.println(i);
+        }
+        */
+
+        // contains function to check how many elements are there in list.
+    }
+}
