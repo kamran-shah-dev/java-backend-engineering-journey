96be3836565be33df981e2b99e273bffb069dc1c
thedomDev <mrbukhari117@gmail.com>
2026-06-14 23:43:46 +0500

    Schedule Thread Pool Executor

--- src/Threads/ExecuterService/ScheduledThreadExecutor.java
+++ src/Threads/ExecuterService/ScheduledThreadExecutor.java
@@ -2,7 +2,6 @@ package Threads.ExecuterService;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class ScheduledThreadExecutor {
